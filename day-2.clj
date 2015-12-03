f612566b8bbc4c91d4da53e7c3f84a3071270501
Bob Williams <bobwilliams.ii@gmail.com>
2015-12-03 10:09:07 -0500

    adding day 2 part two

--- day-2.clj
+++ day-2.clj
@@ -1,5 +1,5 @@
 
-; there's gotta be a better way to do this...could definitely refactor the replace at least
+; there's gotta be a better way to do this...could definitely refactor the string/replace at least
 (defn get-input [file]
   (->> 
     (-> (slurp file)
@@ -22,3 +22,14 @@
 (defn day-two-part-one []
   (let [data (get-input "day-2-input.txt")]
     (reduce + (map #(square-feet (nth % 0) (nth % 1) (nth % 2)) data))))
+
+(defn feet-of-ribbon [l w h]
+  (let [bow (* l w h)
+        order (sort [l w h])
+        a (first order)
+        b (nth order 1)]
+    (+ bow (+ a a b b))))
+
+(defn day-two-part-two []
+  (let [data (get-input "day-2-input.txt")]
+    (reduce + (map #(feet-of-ribbon (nth % 0) (nth % 1) (nth % 2)) data))))
