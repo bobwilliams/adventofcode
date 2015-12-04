a9cf8b4220c9a86d4526396df9a14caf84d0b5ed
Bob Williams <bobwilliams.ii@gmail.com>
2015-12-04 09:28:32 -0500

    right answer, maybe not best solution. time is not on my side :)

--- day-3.clj
+++ day-3.clj
@@ -1,4 +1,6 @@
 
+(def flight-plan (slurp "day-3-input.txt"))
+
 (defn get-new-coords [direction position]
   (cond 
     (= "^" direction) (list (first position) (inc (last position)))
@@ -6,13 +8,13 @@
     (= ">" direction) (list (inc (first position)) (last position))
     :else (list (dec (first position)) (last position))))
 
-(defn day-three-part-one []
-  (loop [homes (slurp "day-3-input.txt") 
+(defn deliver-presents [data visited]
+  (loop [homes data
          num-visited 1
-         homes-visited '((0 0))
+         homes-visited visited
          coords '(0 0)]
     (if (empty? homes)
-      num-visited
+      homes-visited
       (let [fly-to (first homes)
             new-coords (get-new-coords (str fly-to) coords)
             was-visited (not (empty? (some #{new-coords} homes-visited)))]
@@ -21,3 +23,11 @@
           (if was-visited num-visited (inc num-visited))
           (if was-visited homes-visited (cons new-coords homes-visited))
           new-coords)))))
+
+(defn day-three-part-one []
+  (count (deliver-presents flight-plan '((0 0)))))
+
+(defn day-three-part-two []
+  (let [santas-run (deliver-presents (take-nth 2 flight-plan) '((0 0))) 
+        robo-santas-run (deliver-presents (take-nth 2 (rest flight-plan)) santas-run)]
+    (count robo-santas-run)))
