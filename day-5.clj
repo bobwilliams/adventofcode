1110506e4f347bfbdc6dd0da72942c588a275a76
Bob Williams <bobwilliams.ii@gmail.com>
2015-12-06 16:09:02 -0500

    code refactor to exclusively use regex and adding part 2

--- day-5.clj
+++ day-5.clj
@@ -2,32 +2,27 @@
   (-> (slurp "day-5-input.txt")
       (clojure.string/split #"\n")))
 
-(defn at-least-three-vowels? [s]
-  (let [results (for [vowel '(\a \e \i \o \u)]
-                  (map #(= vowel %) s))]
-    (->> results
-         (flatten)
-         (filter true?)
-         (count)
-         (< 2))))
+(defn satisfies-regex? [re s]
+  (not (nil? (re-find re s))))
 
-(defn one-letter-twice-in-a-row? [s]
-  (not (nil? (re-find #"(\w)\1+" s))))
+(def at-least-three-vowels (re-pattern #"[aeiou].*[aeiou].*[aeiou]"))
+(def one-twice-in-a-row (re-pattern #"(.)\1"))
+(def forbiddens (re-pattern #"^(?!.*(ab|cd|pq|xy))")) 
+(def two-twice-no-overlapping  (re-pattern #"(..).*\1" ))
+(def one-repeat-one-between (re-pattern #"(.).\1"))
 
-(defn no-naughties? [s]
-  (let [results (for [naughty ['(\a \b) '(\c \d) '(\p \q) '(\x \y)]]
-                  (map #(= naughty % ) (partition 2 1 s)))]
-    (->> results
-        (flatten)
-        (filter true?)
-        (count)
-        (= 0))))
+(defn nice? [s regexes]
+  (->> regexes
+       (map #(satisfies-regex? % s))
+       (every? true?)))
 
-(defn nice-string? [s]
-  (and 
-    (at-least-three-vowels? s) 
-    (one-letter-twice-in-a-row? s) 
-    (no-naughties? s)))
+(defn how-many-nice [regexes]
+  (count (filter #(nice? % regexes) data)))
 
 (defn day-5-part-one []
-  (count (filter #(nice-string? %) data)))
+  (let [regexes [at-least-three-vowels one-twice-in-a-row forbiddens]]
+    (how-many-nice regexes)))
+
+(defn day-5-part-two []
+  (let [regexes [two-twice-no-overlapping one-repeat-one-between]]
+    (how-many-nice regexes)))
