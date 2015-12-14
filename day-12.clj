74f8a672756351c0885c0b3f15487ad676fa69c8
Bob Williams <bobwilliams.ii@gmail.com>
2015-12-14 16:03:12 -0500

    forgot to check in some parts

--- day-12.clj
+++ day-12.clj
@@ -2,3 +2,11 @@
   (-> "day-12-input.txt"
       (slurp)
       (clojure.string/split-lines)))
+
+(def all-numbers
+  (->> input
+       (map #(re-seq #"-*\d+" %))
+       (flatten)))
+
+(defn day-12-part-one []
+  (reduce + (map read-string all-numbers)))
