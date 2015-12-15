48224f96ce4594cdb8a70d5d79862aba88d4a917
Bob Williams <bobwilliams.ii@gmail.com>
2015-12-14 22:58:09 -0500

    code refactor for day-12.clj

--- day-12.clj
+++ day-12.clj
@@ -1,12 +1,6 @@
-(def input 
-  (-> "day-12-input.txt"
-      (slurp)
-      (clojure.string/split-lines)))
-
-(def all-numbers
-  (->> input
-       (map #(re-seq #"-*\d+" %))
-       (flatten)))
+(def input (slurp "day-12-input.txt"))
+(def all-numbers (re-seq #"-*\d+" input ))
 
 (defn day-12-part-one []
   (reduce + (map read-string all-numbers)))
+
