7fb074c42c6ec9afdf9cba55833dd31a9e1723bc
Bob Williams <bobwilliams.ii@gmail.com>
2015-12-16 14:49:16 -0500

    adding day 12

--- day-12.clj
+++ day-12.clj
@@ -1,6 +1,22 @@
+; tried to find a regex way to do this for part 2...thing I was close but ultimately fell back to json
+(:require [cheshire.core :as json])
+
 (def input (slurp "day-12-input.txt"))
 (def all-numbers (re-seq #"-*\d+" input ))
+
+(defn sum [prev json]
+  (+ prev
+     (cond
+       (or (seq? json) (vector? json)) (reduce sum 0 json)
+       (map? json) (if-not (contains? (set (vals json)) "red")
+                     (reduce sum 0 (vals json))
+                     0)
+       (number? json) json
+       :else 0)))
 
 (defn day-12-part-one []
   (reduce + (map read-string all-numbers)))
 
+(defn day-12-part-two []
+  (let [data (json/parse-string input)]
+    (reduce sum 0 data)))
---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject adventofcode "0.1.0-SNAPSHOT"
+  :description "solutions to adventofcode.com"
+  :url "http://adventofcode.com"
+  :dependencies [[cheshire "5.5.0"]])
